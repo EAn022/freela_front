9c939c8544e7103602bf3fc31d9284365b7d0ab2
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2024-11-04 17:30:20 -0300

    update: component my-profile

--- src/app/components/home-profissional/home-profissional.component.ts
+++ src/app/components/home-profissional/home-profissional.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CardServicesComponent } from '../card-services/card-services.component';
+import { MyProfileComponent } from '../my-profile/my-profile.component';
+import { ProposalComponent } from '../proposal/proposal.component';
+
 
 @Component({
   selector: 'app-home-profissional',
   standalone: true,
   imports: [
     NavbarComponent,
-    CardServicesComponent
+    CardServicesComponent,
+    MyProfileComponent,
+    ProposalComponent
   ],
   templateUrl: './home-profissional.component.html',
   styleUrl: './home-profissional.component.scss'
 })
 export class HomeProfissionalComponent {
-  currentLink: string = 'home'; // Inicialmente, 'home' é o ativo
+  currentLink: string = 'perfil'; // Inicialmente, 'home' é o ativo
 
   setActive(link: string) {
     this.currentLink = link;
