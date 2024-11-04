57855c7933e6bf2c61c739ac9010880553884c4e
Oldiney Galvão <oldiney15@gmail.com>
2024-11-04 10:00:46 -0300

    Add home page admin

---
+++ src/app/components/home-page-admin/home-page-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page-admin.component.html',
+  styleUrl: './home-page-admin.component.scss'
+})
+export class HomePageAdminComponent {
+
+}
