2d4fcb7784774c768ed51cce996e0617a82d9067
Oldiney Galvão <oldiney15@gmail.com>
2024-11-05 09:35:47 -0300

    adição de rotas home-admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeProfissionalComponent } from './components/home-profissional/home-profissional.component';
+import { HomePageAdminComponent } from './components/home-page-admin/home-page-admin.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeProfissionalComponent}
+    {path: '', component: HomeProfissionalComponent},
+    {path: 'page-admin',component:HomePageAdminComponent}
 ];
--- src/app/components/home-page-admin/home-page-admin.component.ts
+++ src/app/components/home-page-admin/home-page-admin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page-admin',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home-page-admin.component.html',
   styleUrl: './home-page-admin.component.scss'
 })
