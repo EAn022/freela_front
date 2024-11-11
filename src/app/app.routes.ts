f053bc91e0de79c957b38f654a4096af1eb66b1a
Dan1ell1 <daniellisz.silva@gmail.com>
2024-11-11 12:07:15 -0300

    update: cadastro responsivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { HomeProfissionalComponent } from './components/home-profissional/home-profissional.component';
 import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
     {path: 'login', component:  LoginComponent},
+    {path: 'cadastro', component: CadastroComponent},
     {path: 'home/profissional', component: HomeProfissionalComponent}
 ];
