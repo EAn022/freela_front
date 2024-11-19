55a86ad9bc824449cd61535e6db5a7642a98b13f
Oldiney Galvão <oldiney15@gmail.com>
2024-11-19 10:20:16 -0300

    rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,18 @@ import { MyProfileComponent } from './components/my-profile/my-profile.component
 import { GerenciamentoComponent } from './components/gerenciamento/gerenciamento.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ProposalComponent } from './components/proposal/proposal.component';
+import { MyProposalComponent } from './components/my-proposal/my-proposal.component';
 
 export const routes: Routes = [
    
-    {path: '', component: HomePageAdminComponent},
-    {path: 'gerenciamento', component: GerenciamentoComponent},
-    {path: 'landing', component: LandingPageComponent},
+    {path: '', component: LandingPageComponent},
     {path: 'login', component:  LoginComponent},
-    {path: 'cadastro', component: CadastroComponent},
+    {path: 'register', component: CadastroComponent},
     {path: 'home/profissional', component: HomeProfissionalComponent},
-    {path: '**', redirectTo: ''}
+    {path: 'admin/home', component: HomePageAdminComponent},
+    {path: 'admin/gerenciamento', component: GerenciamentoComponent},
+    {path: 'profile', component: MyProfileComponent},
+    {path: 'proposal/create', component: ProposalComponent},
+    {path: 'profile/my-proposal', component: MyProposalComponent}
 ];
